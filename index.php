cb0a75e0d1c71381ec2dcea4573a4170f78ffaf0
Vincent <Vincent@Vincent-PC>
2012-06-05 23:31:13 +0200

    maj

--- index.php
+++ index.php
@@ -14,13 +14,6 @@
 	
 		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-	
-		<!-- Le fav and touch icons -->
-		<link rel="shortcut icon" href="./ico/favicon.ico">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="./ico/apple-touch-icon-144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="./ico/apple-touch-icon-114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="./ico/apple-touch-icon-72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="./ico/apple-touch-icon-57-precomposed.png">
 	</head>
 
 	<body>
@@ -95,7 +88,6 @@
 			<!-- Le javascript a la fin pour charger plus vite -->
 			<script src="./js/jQuery.min.js"></script>
 			<script src="./js/bootstrap.min.js"></script>
-			<script src="./js/perso.js"></script>
 		</div>
 	</body>
 </html>
