8f0e4bc13bbcabae466924bd3b3bf1b9ed99b9fa
Vincent <Vincent@Vincent-PC>
2012-08-03 14:56:27 +0200

    maj fixe

---
+++ include/social_network.php
@@ -0,0 +1,15 @@
+<table>
+	<tr>
+		<td><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.vincent-marchal.fr" data-via="marchal_vin" data-lang="fr" data-related="marchal_vin">Tweeter</a></td>
+		<td><g:plusone size="medium"></g:plusone></td>
+		<td class="valign-top"><div class="fb-like" data-href="http://www.vincent-marchal.fr/" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div></td>
+		<td><script type="IN/Share" data-url="http://www.vincent-marchal.fr/" data-counter="right"></script></td>
+	</tr>
+</table>
+
+<!-- Les scripts ensemble -->
+<script type="text/javascript">
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+	window.___gcfg={lang:'fr'};(function(){var po=document.createElement('script');po.type='text/javascript';po.async=true;po.src='https://apis.google.com/js/plusone.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(po,s);})();
+	(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/fr_FR/all.js#xfbml=1";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));
+</script>
